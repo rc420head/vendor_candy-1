12e3621f8fe198ba08316d2c1f7e5c99eaa8598c
Beru Hinode <windowz414@1337.lgbt>
2022-07-07 17:52:00 +0530

    vendor: Add our ringtone and set as default
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,9 +1,6 @@
 # Inherit common mobile CandyRoms stuff
 $(call inherit-product, vendor/candy/config/common.mk)
 
-# Include AOSP audio files
-include vendor/candy/config/aosp_audio.mk
-
 # Themes
 PRODUCT_PACKAGES += \
     ThemePicker \
