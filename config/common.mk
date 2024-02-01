c82e206c0718f8e2919a1a524be280cbd5c0efb2
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2024-02-01 23:10:37 +0530

    Build some packages

--- config/common.mk
+++ config/common.mk
@@ -215,3 +215,8 @@ include vendor/candy/config/version.mk
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=vibe.ogg \
     ro.config.alarm_alert=MorningAlarm.ogg
+
+# Required packages
+PRODUCT_PACKAGES += \
+    Aperture \
+    CandyWallpaperStub
