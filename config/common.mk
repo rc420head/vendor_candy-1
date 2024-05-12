ProjectBlaze/vendor_blaze

vendor: Include packages.mk
*That's why GameSpace wasn't getting builded :(

Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

config/common.mk
@@ -289,3 +289,4 @@ PRODUCT_PACKAGES += \
     BlazeWallpaperStub
 
 include vendor/blaze/config/pixel_props.mk
+include vendor/blaze/config/packages.mk

rc420head/vendor_candy-1

config/common.mk
@@ -1,4 +1,4 @@
-\# Allow vendor/extra to override any property by setting it first
+\\# Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
 PRODUCT_BRAND ?= CandyRoms
@@ -270,3 +270,4 @@ PRODUCT_PACKAGES += \
     CandyWallpaperStub
 
 include vendor/candy/config/pixel_props.mk
+include vendor/candy/config/packages.mk