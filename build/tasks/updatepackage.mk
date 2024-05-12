zaidkhan0997/vendor_blaze

tasks: add fastboot target
* updatepackage but renames to match Blaze target zip
* show additonal info

Co-authored-by: aswin7469 <aswinas@pixysos.com>
Co-authored-by: Sourajit Karmakar <sourajitk@statixos.com>
Change-Id: I5508a4559697c1c707f8414a00f52b6f839fd9b0
Signed-off-by: optimus9811 <vikasyaduvanshi@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
Signed-off-by: Joker-V2 <nipinna0@gmail.com>
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

build/tasks/updatepackage.mk
@@ -1,0 +1,28 @@
+# Copyright (C) 2022 PixysOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# SuperiorOS OTA update package
+
+BLAZE_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/$(BLAZE_VERSION)-updateimages.zip
+
+.PHONY: updatepackage
+updatepackage: $(INTERNAL_UPDATE_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(BLAZE_TARGET_UPDATEPACKAGE)
+	@echo ""
+	@echo ""
+	@echo "**************************************************"
+	@echo " OTA update package is ready: $(BLAZE_TARGET_UPDATEPACKAGE)"
+	@echo "**************************************************"
+	@echo ""

rc420head/vendor_candy-1

build/tasks/updatepackage.mk
@@ -1,0 +1,28 @@
+# Copyright (C) 2022 PixysOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# CandyRoms OTA update package
+
+CANDY_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/$(CANDY_VERSION)-updateimages.zip
+
+.PHONY: updatepackage
+updatepackage: $(INTERNAL_UPDATE_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(CANDY_TARGET_UPDATEPACKAGE)
+	@echo ""
+	@echo ""
+	@echo "**************************************************"
+	@echo " OTA update package is ready: $(CANDY_TARGET_UPDATEPACKAGE)"
+	@echo "**************************************************"
+	@echo ""