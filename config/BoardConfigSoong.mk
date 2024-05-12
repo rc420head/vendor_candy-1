ProjectBlaze/vendor_blaze

soong: Add TARGET_USES_OPLUS_CAMERA
Change-Id: Ifb3ff3a1141712a255640676d24b018ae516add8
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

config/BoardConfigSoong.mk
@@ -50,7 +50,8 @@ SOONG_CONFIG_blazeGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -80,6 +81,7 @@ SOONG_CONFIG_blazeQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRAT
 SOONG_CONFIG_blazeQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_blazeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

rc420head/vendor_candy-1

config/BoardConfigSoong.mk
@@ -50,7 +50,8 @@ SOONG_CONFIG_candyGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += candyNvidiaVars
 SOONG_CONFIG_candyNvidiaVars += \
@@ -80,6 +81,7 @@ SOONG_CONFIG_candyQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRAT
 SOONG_CONFIG_candyQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_candyQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_candyGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_candyGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0